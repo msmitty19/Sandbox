32efc3cdf3b017c2f10742a7b4679f99e66dfb73
Smith <mgavin52828@gmail.com>
2026-01-06 13:49:40 -0500

    Created some Variables and called them into my Print Function

--- Sandbox.py
+++ Sandbox.py
@@ -1,5 +1,11 @@
 # Programmer: Matthew Smith
 # Program: Sandbox
-# Date: 12.18.25
+# Date: 1.6.26
 
-print("This is my amazing sandbox for me to play, learn, make mistakes, and learn some more")
+print("Lets make some Variables!")
+
+year = 2026
+name = "Matthew" 
+age = 16
+
+print("My name is" ,name, "it is the year" ,year, "and I am" ,age, "years old!")
